79fd12b1bc2cf0c533bfb810d854b5dd1642429f
Bridget Burnett <sscott2203@gmail.com>
2018-05-31 16:24:17 +0300

    added photo upload

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,8 +1,8 @@
 class Cocktail < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 
   validates :name, uniqueness: true, presence: true
-  validates :image_url, presence: true
-  mount_uploader :photo, PhotoUploader
+  validates :photo, presence: true
 end
