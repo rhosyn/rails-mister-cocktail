1f2ff10daeab22bcb772ddbe3f5a5d80afd359dd
Bridget Burnett <sscott2203@gmail.com>
2018-05-31 13:16:08 +0300

    amended migrations

--- db/migrate/20180530131229_remove_image_from_cocktail.rb
+++
@@ -1,5 +0,0 @@
-class RemoveImageFromCocktail < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :cocktails, :image, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2018_05_31_095939) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "image"
     t.text "image_url"
   end
 
